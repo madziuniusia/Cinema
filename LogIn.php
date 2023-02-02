46ab83bcfc32fce605d472c060bdee96eba0cef4
Madziuniusia <ola75magda@gmail.com>
2023-02-02 18:15:08 +0100

    first commit

---
+++ LogIn.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if (!isset($_POST['login']) || !isset($_POST['password'])) {
+    header('Location: index.php');
+    exit();
+}
+
+require_once 'Connect.php';
+$connect = @new mysqli($host, $db_user, $db_password, $db_name);
+
+if ($connect->connect_errno != 0) {
+    echo 'Error: ' . $connect->connect_errno;
+} else {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+    $password = htmlentities($password, ENT_QUOTES, "UTF-8");
+
+
+    if ($result = @$connect->query(sprintf(
+        "SELECT * FROM login WHERE login='%s' AND password='%s'",
+        mysqli_real_escape_string($connect, $login),
+        mysqli_real_escape_string($connect, $password)
+    ))) {
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $_SESSION['user'] = $row['Login'];
+            $_SESSION['LogIn'] = true;
+            $_SESSION['id'] = $row['IDlogin'];
+            $result->free_result();
+            header('Location: CinemaReservation.php');
+        } else {
+            $_SESSION['error'] = true;
+            header('Location: index.php');
+        }
+    }
+    $connect->close();
+}
